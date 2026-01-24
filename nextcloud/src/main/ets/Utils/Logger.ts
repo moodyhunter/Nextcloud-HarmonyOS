5e4b9ba2608b5a1613cb75ee715da9b198a809fe
Moody Liu <mooodyhunter@outlook.com>
2026-01-25 03:28:55 +0800

    uploader & job manager

--- nextcloud/src/main/ets/Utils/Logger.ts
+++ nextcloud/src/main/ets/Utils/Logger.ts
@@ -1,22 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-import hilog from '@ohos.hilog'
+import hilog from '@ohos.hilog';
 
 const DOMAIN = 0xABAB;
+const NEXTCLOUD_TAG = 'NextcloudApp';
 
 export class Logger {
     static Debug(component: string, ...message: string[]) {
-        hilog.debug(DOMAIN, component, message.join(', '))
+        hilog.debug(DOMAIN, NEXTCLOUD_TAG, `[${component}] ` + message.join(', '))
     }
 
     static Info(component: string, ...message: string[]) {
-        hilog.info(DOMAIN, component, message.join(', '))
+        hilog.info(DOMAIN, NEXTCLOUD_TAG, `[${component}] ` + message.join(', '))
     }
 
     static Warn(component: string, ...message: string[]) {
-        hilog.warn(DOMAIN, component, message.join(', '))
+        hilog.warn(DOMAIN, NEXTCLOUD_TAG, `[${component}] ` + message.join(', '))
     }
 
     static Error(component: string, ...message: string[]) {
-        hilog.error(DOMAIN, component, message.join(', '))
+        hilog.error(DOMAIN, NEXTCLOUD_TAG, `[${component}] ` + message.join(', '))
     }
 }
