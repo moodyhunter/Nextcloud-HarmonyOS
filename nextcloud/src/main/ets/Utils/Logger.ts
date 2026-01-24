0a00175f9ded8c3186e070e20624d6eee91d9b72
Moody <mooodyhunter@outlook.com>
2026-01-24 17:17:29 +0800

    temp

--- nextcloud/src/main/ets/Utils/Logger.ts
+++ nextcloud/src/main/ets/Utils/Logger.ts
@@ -4,19 +4,19 @@ import hilog from '@ohos.hilog'
 const DOMAIN = 0xABAB;
 
 export class Logger {
-  static Debug(component: string, message: string) {
-    hilog.debug(DOMAIN, component, message)
-  }
+    static Debug(component: string, ...message: string[]) {
+        hilog.debug(DOMAIN, component, message.join(', '))
+    }
 
-  static Info(component: string, message: string) {
-    hilog.info(DOMAIN, component, message)
-  }
+    static Info(component: string, ...message: string[]) {
+        hilog.info(DOMAIN, component, message.join(', '))
+    }
 
-  static Warn(component: string, message: string) {
-    hilog.warn(DOMAIN, component, message)
-  }
+    static Warn(component: string, ...message: string[]) {
+        hilog.warn(DOMAIN, component, message.join(', '))
+    }
 
-  static Error(component: string, message: string) {
-    hilog.error(DOMAIN, component, message)
-  }
+    static Error(component: string, ...message: string[]) {
+        hilog.error(DOMAIN, component, message.join(', '))
+    }
 }
